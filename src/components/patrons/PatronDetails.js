f538f92c54149ef5fb6716a0613f63d98fe7554a
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-14 15:37:53 -0600

    patron details view works

--- src/components/patrons/PatronDetails.js
+++ src/components/patrons/PatronDetails.js
@@ -1,3 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { getPatronById } from "../../data/patronsData";
+import { Table } from "reactstrap";
+
 export const PatronDetails = () => {
-    return <h1>Patron Details</h1>
+    const {id} = useParams();
+    const [ patron, setPatron] = useState({});
+    
+    let lateFees = 0;
+    patron.checkouts?.map(co => lateFees += co.lateFee)
+
+    useEffect(()=> {
+        getPatronById(id * 1).then(data => setPatron(data[0]));
+    }, [id]);
+    
+    if (!patron) {
+        return null;
+    }
+    
+    return (
+    <div className="container">
+        <h2>{patron.firstName} {patron.lastName}</h2>
+        <Table>
+        <tbody>
+            <tr>
+                <th scope="row">Email</th>
+                <td>{patron.email}</td>
+            </tr>
+            <tr>
+                <th scope="row">Address</th>
+                <td>{patron.address}</td>
+            </tr>
+            <tr>
+                <th scope="row">Late Fees?</th>
+                <td>${lateFees}.00</td>
+            </tr>
+        </tbody>
+        </Table>
+    </div>);
 }
--- src/data/patronsData.js
+++ src/data/patronsData.js
@@ -2,4 +2,8 @@ const _apiUrl = "/api/patrons";
 
 export const GetPatrons = () => {
     return fetch(_apiUrl).then(r => r.json());
-}
+};
+
+export const getPatronById = (id) => {
+    return fetch(`${_apiUrl}/${id * 1}`).then(r => r.json());
+};
